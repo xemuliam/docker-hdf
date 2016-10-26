1ac16a8b7c016038b7f1c4a4e646048ce6f06e44
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-10-26 11:57:30 +0300

    2.0

--- start_hdf.sh
+++ start_hdf.sh
@@ -3,43 +3,47 @@
 set -e
 
 do_site2site_configure() {
-  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=10101/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=2881/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g"  ${HDF_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
-# NiFi properties
-  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=10201/g" ${HDF_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${ZK_NODES_LIST}/g" ${HDF_HOME}/conf/nifi.properties
-  if [ "$USE_EMBEDDED_ZK" == "true" ]; then
-    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${HDF_HOME}/conf/nifi.properties
+  ZK_NODES=$(echo ${ZK_NODES_LIST} | sed -e "s/\s/,/g" -e "s/\(,\)*/\1/g")
+
+  if [ -z "$ZK_CLIENT_PORT" ]; then ZK_CLIENT_PORT=2181; fi
+  sed -i "s/clientPort=.*/clientPort=${ZK_CLIENT_PORT}/g"  ${HDF_HOME}/conf/zookeeper.properties
+  ZK_CONNECT_STRING=$(echo $ZK_NODES | sed -e "s/,/:${ZK_CLIENT_PORT},/g" -e "s/$/:${ZK_CLIENT_PORT}/g")
+
+  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=2882/g"  ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=$ZK_CONNECT_STRING/g"  ${HDF_HOME}/conf/nifi.properties
+
+  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">$ZK_CONNECT_STRING</g"  ${HDF_HOME}/conf/state-management.xml
+ 
+  if [ ! -z "$ZK_MYID" ]; then
+    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g"  ${HDF_HOME}/conf/nifi.properties
+
+    mkdir -p  ${HDF_HOME}/state/zookeeper
+    echo ${ZK_MYID} >  ${HDF_HOME}/state/zookeeper/myid
   else 
-    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=true/nifi.state.management.embedded.zookeeper.start=false/g" ${HDF_HOME}/conf/nifi.properties
+    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=true/nifi.state.management.embedded.zookeeper.start=false/g"  ${HDF_HOME}/conf/nifi.properties
   fi
 
-# State management
-  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${ZK_NODES_LIST}</g" ${HDF_HOME}/conf/state-management.xml
-
-# MyId zookeeper
-  if [ "$USE_EMBEDDED_ZK" == "true" ]; then
-    mkdir -p ${HDF_HOME}/state/zookeeper
-    echo ${MYID} > ${HDF_HOME}/state/zookeeper/myid
-  fi
-
-# Zookeeper properties
-#  sed -i "/^server\.1=/q" ${HDF_HOME}/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${HDF_HOME}/conf/zookeeper.properties
+  if [ -z "$ZK_INTERCOM_PORT" ]; then ZK_INTERCOM_PORT=2888; fi
+  if [ -z "$ZK_ELECTION_PORT" ]; then ZK_ELECTION_PORT=3888; fi
+  sed -i "/^server\./,$ d"  ${HDF_HOME}/conf/zookeeper.properties
+  srv=1; IFS=","; for node in $ZK_NODES; do sed -i "\$aserver.$srv=$node:${ZK_INTERCOM_PORT}:${ZK_ELECTION_PORT}"  ${HDF_HOME}/conf/zookeeper.properties; let "srv+=1"; done
 }
 
 do_site2site_configure
 
-if [ "$INSTANCE_ROLE" == "cluster-node" ]; then
+if [ ! -z "$IS_CLUSTER_NODE" ]; then
   do_cluster_node_configure
 fi
 
-tail -F ${HDF_HOME}/logs/nifi-app.log &
-${HDF_HOME}/bin/nifi.sh run
+tail -F  ${HDF_HOME}/logs/nifi-app.log &
+ ${HDF_HOME}/bin/nifi.sh run
